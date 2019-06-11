4ad4ed1b711c7732180660f2dac7ea786eaa8adf
Matias Menker <menkermatias@gmail.com>
2019-06-11 10:11:44 +0200

    call first render switch period with periods
    - call first render switch period with periods
    - add test mock switchperiod
    - add test switchperiod

--- src/stories/SwitchPeriodComparative.stories.js
+++ src/stories/SwitchPeriodComparative.stories.js
@@ -7,10 +7,12 @@ import Cell from 'components/Cell';
 import Title from 'components/Title';
 import Theme from 'components/Theme';
 
-const startDate = '2019-05-31T00:00:00+02:00';
-const endDate = '2019-06-06T23:59:59+02:00';
-const previousPriodText = 'Previous period';
-const samePriodLastYearText = 'Same period last year';
+import {
+  startDate,
+  endDate,
+  previousPriodText,
+  samePriodLastYearText,
+} from 'test/mock/SwitchPeriodComparative';
 
 storiesOf('SwitchPeriodComparative', module)
   .addParameters({
@@ -29,6 +31,18 @@ storiesOf('SwitchPeriodComparative', module)
             previousPriodText={previousPriodText}
             samePriodLastYearText={samePriodLastYearText}
           />
+        </Cell>
+      </Grid>
+    </Theme>
+  ))
+  .add('without text', () => (
+    <Theme>
+      <Grid type="horizontal" horizontalPadding verticalPadding>
+        <Cell size="full">
+          <Title text="Basic SwitchPeriodComparative:" line />
+        </Cell>
+        <Cell size="auto" medium={12}>
+          <SwitchPeriodComparative startDate={startDate} endDate={endDate} />
         </Cell>
       </Grid>
     </Theme>
---
+++ test/mock/SwitchPeriodComparative.js
@@ -0,0 +1,11 @@
+const startDate = '2019-05-31T00:00:00+02:00';
+const endDate = '2019-06-06T23:59:59+02:00';
+const previousPriodText = 'Previous period';
+const samePriodLastYearText = 'Same period last year';
+
+export {
+  startDate,
+  endDate,
+  previousPriodText,
+  samePriodLastYearText
+};
