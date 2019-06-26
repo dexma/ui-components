9087b20588029433c0076c2e8a44818828e3620a
Matias Menker <menkermatias@gmail.com>
2019-06-26 16:22:33 +0200

    chart styled and deepEqual for update props
    - add styles theme to chart

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -1,13 +1,86 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
+import deepEqual from 'deep-equal';
+import { withTheme } from 'styled-components';
+
 import Highcharts from 'highcharts';
+import addHeatmapModule from 'highcharts/modules/heatmap';
+import addTreemapModule from 'highcharts/modules/treemap';
 import boost from 'highcharts/modules/boost';
 
+import theme from 'styles/theme';
+
 boost(Highcharts);
+addHeatmapModule(Highcharts);
+addTreemapModule(Highcharts);
 
 const propTypes = {
   options: PropTypes.objectOf(PropTypes.any),
   callback: PropTypes.func,
+  theme: PropTypes.shape({}),
+};
+
+const defaultProps = {
+  theme: theme,
+};
+
+const getStyles = () => {
+  const {
+    backgroundColor,
+    /*    backgroundColorActive,
+    backgroundColorFocused,
+    backgroundColorHover,
+    backgroundColorSelected,
+    border,
+    borderRadius,
+    component,
+    fontColor,
+    fontColorActive,
+    fontFamily, */
+    fontSize,
+    /*    fontWeightBold,
+    iconColor,
+    iconColorActive,
+    iconSize,
+    padding, */
+  } = theme;
+  return {
+    chart: {
+      backgroundColor: backgroundColor,
+    },
+    title: {
+      style: {
+        fontWeight: 'bold',
+      },
+    },
+    tooltip: {
+      backgroundColor: backgroundColor,
+      shadow: false,
+    },
+    legend: {
+      backgroundColor: backgroundColor,
+      itemStyle: {
+        fontSize: fontSize,
+      },
+    },
+    xAxis: {
+      labels: {
+        style: {
+          fontSize: fontSize,
+        },
+      },
+    },
+    yAxis: {
+      title: {
+        style: {},
+      },
+      labels: {
+        style: {
+          fontSize: fontSize,
+        },
+      },
+    },
+  };
 };
 
 class Chart extends PureComponent {
@@ -23,6 +96,13 @@ class Chart extends PureComponent {
       options,
       callback || undefined
     );
+    this.chart.update(getStyles());
+  }
+
+  componentDidUpdate(prevProps) {
+    if (!deepEqual(this.props.options, prevProps.options)) {
+      this.chart.update(this.props.options);
+    }
   }
 
   componentWillUnmount() {
@@ -38,5 +118,6 @@ class Chart extends PureComponent {
 }
 
 Chart.propTypes = propTypes;
+Chart.defaultProps = defaultProps;
 
-export default Chart;
+export default withTheme(Chart);
