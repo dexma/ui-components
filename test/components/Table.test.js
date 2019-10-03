78319d42250128e2352d43b0dab4a8a4c94ea12d
Matias Menker <menkermatias@gmail.com>
2019-10-03 18:06:26 +0200

    refactor(components): add props and default props (theme)

--- test/components/Table.test.js
+++ test/components/Table.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { mount } from 'enzyme';
+import { mountWithTheme } from '../utils/wrapperThemeProvider';
 
 import Table from 'components/Table';
 
@@ -39,7 +39,7 @@ const columns = [
 
 describe('<Table>', () => {
   it('Should render the correct component', () => {
-    const table = mount(<Table dataSource={dataSource} columns={columns}/>);
+    const table = mountWithTheme(<Table dataSource={dataSource} columns={columns}/>);
     expect(table.find('StyledTable').length).toEqual(1);
   });
 });
