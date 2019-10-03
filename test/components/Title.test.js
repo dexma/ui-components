ace2895f4edad8588ba1f3d1628888ae1a8ef5d1
Matias Menker <menkermatias@gmail.com>
2019-10-03 16:57:54 +0200

    test(component): add the theme provider wrapper to the tests

--- test/components/Title.test.js
+++ test/components/Title.test.js
@@ -1,31 +1,31 @@
 import React from 'react';
-import { mount } from 'enzyme';
-
 import Title from 'components/Title';
 import Heading from 'components/Heading';
+
+import { mountWithTheme } from '../utils/wrapperThemeProvider';
 
 const text = 'Some text';
 const type = 'h3';
 
 describe('<Title>', () => {
   it('Should render the correct classNamePrefix component', () => {
-    const title = mount(<Title text={text} />);
+    const title = mountWithTheme(<Title text={text} />);
     expect(title.find('StyledTitle').length).toEqual(1);
   });
   it('Should render the correct type', () => {
-    const title = mount(<Title text={text} type={type} />);
+    const title = mountWithTheme(<Title text={text} type={type} />);
     expect(title.find(type).length).toEqual(1);
   });
   it('Should render the correct text', () => {
-    const title = mount(<Title text={text} type={type} />);
+    const title = mountWithTheme(<Title text={text} type={type} />);
     expect(title.find(type).text()).toEqual(text);
   });
   it('Should render the correct line className', () => {
-    const title = mount(<Title text={text} type={type} />);
+    const title = mountWithTheme(<Title text={text} type={type} />);
     expect(title.find('.line').length).toEqual(2);
   });
   it('Should render Heading component', () => {
-    const title = mount(<Title text={text} type={type} />);
+    const title = mountWithTheme(<Title text={text} type={type} />);
     expect(title.find(Heading).length).toEqual(1);
   });
 });
