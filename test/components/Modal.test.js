7e13d8cd2e3fbeceafecf98bf1ffba76b5eab85f
Matias Menker <menkermatias@gmail.com>
2019-10-07 17:02:23 +0200

    refactor(modal): add more tests and props

--- test/components/Modal.test.js
+++ test/components/Modal.test.js
@@ -1,18 +1,55 @@
 import React from 'react';
-import { cleanup, render } from '@testing-library/react';
-
 import Modal from 'components/Modal';
-import Theme from 'components/Theme';
+import { mount } from 'enzyme';
 
 describe('<Modal>', () => {
-  afterEach(cleanup);
-
-  test('render Modal', () => {
-    const { container } = render(
-      <Theme>
-        <Modal />
-      </Theme>,
+  it('Should render', () => {
+    const modal = mount(<Modal visible />);
+    expect(modal.find('StyledModal').length).toEqual(1);
+  });
+  it('Should have the container class', () => {
+    const modal = mount(<Modal visible />);
+    expect(modal.find('.container').length).toEqual(1);
+    expect(modal.find('.panel').length).toEqual(1);
+    expect(modal.find('.mask').length).toEqual(1);
+  });
+  it('Should have the containerClass class', () => {
+    const modal = mount(<Modal visible={false} />);
+    expect(modal.find('.containerHidden').length).toEqual(1);
+    expect(modal.find('.panelHidden').length).toEqual(1);
+    expect(modal.find('.maskHidden').length).toEqual(1);
+  });
+  it('Should call function on click outside', () => {
+    const mockCallBack = jest.fn();
+    const modal = mount(<Modal onClickAway={mockCallBack} />);
+    modal.find('.maskHidden').simulate('click');
+    expect(mockCallBack.mock.calls.length).toEqual(1);
+  });
+  it('Should render correct children', () => {
+    const modal = mount(
+      <Modal>
+        <h1 className="element-test">Hello Test!</h1>
+      </Modal>
     );
-    expect(container.firstChild).toMatchSnapshot()
+    expect(modal.find('.element-test').length).toEqual(1);
+  });
+  it('Should hide when press Escape key', () => {
+    const map = {
+      keydown: () => {},
+    };
+    window.addEventListener = jest.fn((event, cb) => {
+      map[event] = cb;
+    });
+    const modal = mount(
+      <Modal visible>
+        <h1 className="element-test">Hello Test!</h1>
+      </Modal>
+    );
+    expect(modal.find('Modal').state('visible')).toBeTruthy();
+    map.keydown({ key: 'Escape' });
+    expect(modal.find('Modal').state('visible')).toBeFalsy();
+    modal.find('Modal').setState({ visible: true });
+    map.keydown({ key: 'Enter' });
+    expect(modal.find('Modal').state('visible')).toBeTruthy();
   });
 });
