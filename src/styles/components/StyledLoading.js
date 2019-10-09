57c2c055238016a677ed22ad08fcf8c501fd72dd
Matias Menker <menkermatias@gmail.com>
2019-10-09 11:07:23 +0200

    refactor(loading): add styled and remove scss

--- src/styles/components/StyledLoading.js
+++ src/styles/components/StyledLoading.js
@@ -1,4 +1,6 @@
-.dexma-loading{
+import styled from 'styled-components';
+
+const StyledLoading = styled.div`
   position: absolute;
   width: 100%;
   height: 100%;
@@ -6,8 +8,10 @@
   left: 0;
   bottom: 0;
   right: 0;
-  z-index: 9;
+  z-index: 999999999;
   justify-content: center;
   display: flex;
   align-items: center;
-}
+`;
+
+export { StyledLoading };
