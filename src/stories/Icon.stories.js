f0a5115819259c145a51ca1e6055f407547bb12a
Matias Menker <menkermatias@gmail.com>
2019-10-28 09:42:03 +0100

    refactor(icons): add search for the icons

--- src/stories/Icon.stories.js
+++ src/stories/Icon.stories.js
@@ -1,6 +1,7 @@
-import React, { Fragment } from 'react';
+import React, { Component, Fragment } from 'react';
 import { storiesOf } from '@storybook/react';
 import icon from 'config/icon';
+import styled from 'styled-components';
 
 import Grid from 'components/Grid';
 import Cell from 'components/Cell';
@@ -8,37 +9,74 @@ import Title from 'components/Title';
 import Paragraph from 'components/Paragraph';
 import Icon from 'components/Icon';
 
-const getIcons = (color, size) => {
-  return icon.map(iconItem => (
-    <Cell medium={3}>
-      <Icon name={iconItem.name} size={size} color={color} />
-      {iconItem.name}
-    </Cell>
-  ));
-};
+const StyledFragment = styled.div`
+  .search {
+    padding: 13px 25px;
+    border: 1px solid grey;
+    border-radius: 4px;
+    width: 400px;
+    display: block;
+    margin: 25px auto 50px auto;
+  }
+`;
+
+class SearchIcons extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: '',
+    };
+  }
+
+  handleChange = e => {
+    this.setState({
+      value: e.target.value,
+    });
+  };
+
+  getIcons = searchValue => {
+    const iconsFilter = icon.filter(i => i.name.includes(searchValue));
+    return iconsFilter.map(iconItem => (
+      <Cell medium={3}>
+        <Icon name={iconItem.name} size="large" color="gray500" />
+        {iconItem.name}
+      </Cell>
+    ));
+  };
+
+  render() {
+    const { value } = this.state;
+    return (
+      <Fragment>
+        <Grid type="container">
+          <Cell size="full">
+            <input
+              className="search"
+              type="text"
+              onChange={this.handleChange}
+            />
+          </Cell>
+          <Cell size="full">
+            <Grid type="horizontal">{this.getIcons(value)}</Grid>
+          </Cell>
+        </Grid>
+      </Fragment>
+    );
+  }
+}
 
 storiesOf('Icon', module)
   .addParameters({
     jest: ['Icon'],
   })
   .add('basic', () => (
-    <Fragment>
+    <StyledFragment>
       <Grid type="horizontal" horizontalPadding verticalPadding>
         <Cell size="full">
           <Title text="Icons" line={false} type="h4" />
-          <Paragraph>
-            Buttons communicate and trigger actions a user can take in the
-            application. A button should represent an action a user can take,
-            like submitting a form, cancelling a process, or creating a new
-            object. When the user clicks a button, the associated action should
-            be triggered or performed immediately.
-          </Paragraph>
+          <Paragraph>Icons with svg</Paragraph>
         </Cell>
-        <Grid type="horizontal">
-          <Cell medium={10} mediumOffset={1}>
-            <Grid type="horizontal">{getIcons('gray500', 'large')}</Grid>
-          </Cell>
-        </Grid>
+        <SearchIcons />
       </Grid>
-    </Fragment>
+    </StyledFragment>
   ));
