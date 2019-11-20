5ae7d3820549843885146263f6d437c60dbdc92b
Matias Menker <menkermatias@gmail.com>
2019-11-20 21:06:31 +0100

    update(button): update button stories

--- src/.storybook/config.js
+++ src/.storybook/config.js
@@ -5,7 +5,8 @@ import { withA11y } from '@storybook/addon-a11y';
 import { withTests } from '@storybook/addon-jest';
 import results from 'test/jest-test-results';
 
-//https://github.com/storybooks/storybook/tree/master/addons/info
+import { GlobalStyle, StorybookStyles } from 'styles/global';
+
 addDecorator(withA11y);
 
 addDecorator(
@@ -14,7 +15,14 @@ addDecorator(
   })
 );
 
-// automatically import all files ending in *.stories.js
+addDecorator(story => (
+  <>
+    <GlobalStyle />
+    <StorybookStyles />
+    {story()}
+  </>
+));
+
 configure(
   [
     require.context('../stories', true, /\.stories\.mdx$/),
