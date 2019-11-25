197c56c81716cce390ef283d731797011bb6cc11
Matias Menker <menkermatias@gmail.com>
2019-11-25 17:46:15 +0100

    update(loading): update stories

--- src/stories/Loading.stories.js
+++ src/stories/Loading.stories.js
@@ -1,23 +1,30 @@
 import React from 'react';
-import { storiesOf } from '@storybook/react';
-
-import Loading from 'components/Loading';
+import { Loading } from 'components/Loading';
 import Grid from 'components/Grid';
+import Row from 'components/Row';
 import Cell from 'components/Cell';
+import Paragraph from '../components/Paragraph';
 
-storiesOf('Loading', module)
-  .addParameters({
-    jest: ['Loading'],
-  })
-  .add('basic', () => (
-    <Grid type="vertical" horizontalPadding verticalPadding>
-      <Cell size="full"></Cell>
-      <Cell size="auto">
+export default {
+  title: 'Loading',
+  component: Loading,
+};
+
+export const loading = () => (
+  <Grid fluid>
+    <Row>
+      <Cell xs={12}>
+        <Paragraph m="1rem 0 1rem 0">
+          Render loading spinner for the UI views
+        </Paragraph>
+      </Cell>
+      <Cell xs={12}>
         <div
           style={{ background: '#eee', padding: '1.5em', position: 'relative' }}
         >
           <Loading isLoading />
         </div>
       </Cell>
-    </Grid>
-  ));
+    </Row>
+  </Grid>
+);
