f30e2b5d61d3f4acde75da646f102a3f12886507
Matias Menker <menkermatias@gmail.com>
2019-11-26 15:03:34 +0100

    update(table): add selection stories

--- src/stories/Table.stories.js
+++ src/stories/Table.stories.js
@@ -12,6 +12,34 @@ export default {
   component: Table,
 };
 
+class Selection extends React.Component {
+  state = {
+    selectedRowKeys: [], // Check here to configure the default column
+  };
+
+  onSelectChange = selectedRowKeys => {
+    console.log('selectedRowKeys changed: ', selectedRowKeys);
+    this.setState({ selectedRowKeys });
+  };
+
+  render() {
+    const { selectedRowKeys } = this.state;
+    const rowSelection = {
+      selectedRowKeys,
+      onChange: this.onSelectChange,
+    };
+    return (
+      <div>
+        <Table
+          rowSelection={rowSelection}
+          columns={columnsTable}
+          dataSource={dataTable}
+        />
+      </div>
+    );
+  }
+}
+
 export const table = () => (
   <Grid fluid>
     <Row>
@@ -23,6 +51,22 @@ export const table = () => (
       </Cell>
       <Cell xs={12}>
         <Table dataSource={dataTable} columns={columnsTable} />
+      </Cell>
+    </Row>
+  </Grid>
+);
+
+export const selection = () => (
+  <Grid fluid>
+    <Row>
+      <Cell xs={12}>
+        <Paragraph m="1rem 0 1rem 0">
+          Here we have dots for show loading part of the UI you can customize
+          with 2, 3 or 4 dot
+        </Paragraph>
+      </Cell>
+      <Cell xs={12}>
+        <Selection />
       </Cell>
     </Row>
   </Grid>
