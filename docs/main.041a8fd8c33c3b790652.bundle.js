1d1146014d23bdb0be99090fb1a62d585436eaea
Matias Menker <menkermatias@gmail.com>
2019-12-12 11:09:59 -0300

    fix(docs): update docs

---
+++ docs/main.041a8fd8c33c3b790652.bundle.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{0:function(n,o,c){c(532),c(644),c(1150),n.exports=c(1152)},1:function(n,o){},644:function(n,o,c){"use strict";c.r(o);c(645),c(860),c(1003),c(1112)}},[[0,1,2]]]);
