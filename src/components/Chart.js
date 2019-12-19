725f8d82d5212aacb136c16d0b3ac913d4aa3113
Matias Menker <menkermatias@gmail.com>
2019-12-19 12:08:30 -0300

    fix(chart): add export modules highcharts

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -5,12 +5,19 @@ import { withTheme } from 'styled-components';
 
 import Highcharts from 'highcharts';
 import addSankeyModule from 'highcharts/modules/sankey';
+import addExportingModule from 'highcharts/modules/exporting';
+import addOfflineExportingModule from 'highcharts/modules/offline-exporting';
+import addExportData from 'highcharts/modules/export-data';
+
 import boost from 'highcharts/modules/boost';
 
 import theme from 'styles/theme';
 
 boost(Highcharts);
 addSankeyModule(Highcharts);
+addExportingModule(Highcharts);
+addOfflineExportingModule(Highcharts);
+addExportData(Highcharts);
 
 const propTypes = {
   options: PropTypes.objectOf(PropTypes.any),
