e731023571f1bb05680547841acd74511dbac9a7
Christian Martínez <gkhristian@gmail.com>
2021-04-01 14:46:18 +0200

    feat(dataid): add component

---
+++ src/components/DataId/withDataId.js
@@ -0,0 +1,32 @@
+import React, { useContext } from 'react';
+import hoistStatics from 'hoist-non-react-statics';
+
+import { DataIdContext } from './DataIdProvider';
+
+export default (WrappedComponent) => {
+    const withDataId = React.forwardRef((props, ref) => {
+        const context = useContext(DataIdContext);
+        const { defaultProps } = WrappedComponent;
+
+        let dataId = defaultProps?.dataId;
+
+        if (context && props.dataId) {
+            dataId = `${context}.${props.dataId}`;
+        } else if (context) {
+            dataId = `${context}.${dataId}`;
+        } else if (props.dataId) {
+            dataId = props.dataId;
+        }
+
+        const newProps = { ...props, dataId };
+
+        return <WrappedComponent {...newProps} ref={ref} />;
+    });
+
+    const Hoisted = hoistStatics(withDataId, WrappedComponent);
+    Hoisted.defaultProps = WrappedComponent.defaultProps;
+
+    withDataId.displayName = `withDataId(WrappedComponent)`;
+
+    return Hoisted;
+};
