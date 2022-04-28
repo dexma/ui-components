7bb0192934185275a58d1bf315ca254b77c5444a
Christian Martínez <gkhristian@gmail.com>
2022-04-28 18:36:20 +0200

    update docs

--- docs/main.224710de32e85ebd4008.manager.bundle.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{535:function(module,exports,__webpack_require__){__webpack_require__(536),__webpack_require__(943),__webpack_require__(880),__webpack_require__(946),__webpack_require__(888),__webpack_require__(945),__webpack_require__(948),module.exports=__webpack_require__(949)},602:function(module,exports){}},[[535,2,3]]]);
---
+++ docs/main.a3927492f653edd3e761.manager.bundle.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{592:function(module,exports,__webpack_require__){__webpack_require__(593),__webpack_require__(875),__webpack_require__(1150),__webpack_require__(878),__webpack_require__(1186),__webpack_require__(877),__webpack_require__(880),module.exports=__webpack_require__(881)},612:function(module,exports){}},[[592,1,2]]]);
