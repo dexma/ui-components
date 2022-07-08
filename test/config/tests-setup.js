66af63289edab13b74525366249f01bb73a1431f
Luis Valero <luivalgar@gmail.com>
2022-07-08 11:04:29 +0200

    feat(deps): bump antd from 3.16.1 to 4.21.0
    Bump antd from 3.16.1 to 4.21.0 and adapt affected components

--- test/config/tests-setup.js
+++ test/config/tests-setup.js
@@ -6,3 +6,17 @@ configure({ adapter: new Adapter() });
 Object.defineProperty(document, 'currentScript', {
   value: document.createElement('script'),
 });
+
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: jest.fn().mockImplementation(query => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: jest.fn(),
+    removeListener: jest.fn(),
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    dispatchEvent: jest.fn(),
+  })),
+});
