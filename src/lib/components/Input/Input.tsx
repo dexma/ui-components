80739bc4600d7fa4cdfa22dca2ae406fe4aaccd6
matias.pavan1 <matias.pavan@randstad.es>
2025-03-26 16:31:17 +0100

    feat(input): add label tag, add ariaLabel property, change type: string to type: enum.

--- src/lib/components/Input/Input.tsx
+++ src/lib/components/Input/Input.tsx
@@ -11,21 +11,22 @@ type InputProps = {
     placeholder?: string;
     id?: string;
     icon?: string;
-    label?: string | ReactNode;
+    label: string;
     title?: string;
     value?: string;
     isLoading?: boolean;
-    type?: string;
+    type?: 'email' | 'file' | 'number' | 'password' | 'tel' | 'text' | 'url';
     name?: string;
     disabled?: boolean;
     dataId?: string;
     iconAriaLabel?: string;
+    ariaLabel?: string;
 } & InputHTMLAttributes<HTMLInputElement>;
 
 export const Input = withDataId(
     forwardRef((props: InputProps, ref: ForwardedRef<HTMLInputElement>) => {
         const [focused, setFocused] = useState(false);
-        const { icon, isLoading, onFocus, onBlur, children, dataId, iconAriaLabel } = props;
+        const { icon, isLoading, onFocus, onBlur, children, dataId, iconAriaLabel, label, ariaLabel } = props;
         const th = useContext(ThemeContext) || defaultTheme;
         const newProps = omit(props, ['placeholder', 'id', 'label', 'value', 'type', 'name', 'onChange', 'onFocus', 'onBlur', 'dataId', 'isLoading']);
         const inputProps = omit(props, ['icon', 'isLoading', 'theme', 'children', 'onFocus', 'onBlur', 'dataId', 'isLoading']);
@@ -38,17 +39,26 @@ export const Input = withDataId(
             if (onBlur) onBlur(e);
         };
         return (
-            <StyledInput data-testid='input' $icon={icon} $isLoading={isLoading !== undefined ? isLoading : false} $focused={focused} data-id={dataId} theme={th} {...newProps}>
-                {icon && (
-                    <div className='icon-container'>
-                        <Icon name={icon} size={20} color='gray500' ariaLabel={!iconAriaLabel ? `${icon} icon` : iconAriaLabel} />
-                    </div>
-                )}
-                <input onFocus={handleOnFocus} onBlur={handleOnBlur} {...inputProps} ref={ref} data-testid='input-element' />
-                {isLoading && <Spinner size={20} />}
-                {children && children}
-            </StyledInput>
+            <>
+                {getLabel(label, dataId)}
+                <StyledInput data-testid='input' $icon={icon} $isLoading={isLoading !== undefined ? isLoading : false} $focused={focused} data-id={dataId} theme={th} {...newProps}>
+                    {icon && (
+                        <div className='icon-container'>
+                            <Icon name={icon} size={20} color='gray500' ariaLabel={!iconAriaLabel ? `${icon} icon` : iconAriaLabel} />
+                        </div>
+                    )}
+                    <input onFocus={handleOnFocus} onBlur={handleOnBlur} {...inputProps} ref={ref} data-testid='input-element' aria-label={ariaLabel} />
+                    {isLoading && <Spinner size={20} />}
+                    {children && children}
+                </StyledInput>
+            </>
         );
     }),
     'input'
+);
+
+const getLabel = (_label: ReactNode, dataId?: string) => (
+    <label id={dataId} className='sr-only'>
+        {_label}
+    </label>
 );
--- src/stories/Input.stories.tsx
+++ src/stories/Input.stories.tsx
@@ -14,7 +14,7 @@ export const Basic = () => (
                 <Paragraph margin='1rem 0 1rem 0'>Basic input text</Paragraph>
             </Cell>
             <Cell xs={12}>
-                <Input type='text' placeholder='Input text' />
+                <Input type='text' placeholder='Input text' label='Basic input text' />
             </Cell>
         </Row>
     </Grid>
@@ -27,10 +27,10 @@ export const Search = () => (
                 <Paragraph margin='1rem 0 1rem 0'>Input search in two cell</Paragraph>
             </Cell>
             <Cell xs={6}>
-                <Input type='text' placeholder='Search text' icon='search' />
+                <Input type='text' placeholder='Search text' icon='search' label='Search text' ariaLabel='Search' />
             </Cell>
             <Cell xs={6}>
-                <Input type='text' placeholder='Insert date' icon='calendar_range' />
+                <Input type='text' placeholder='Insert date' icon='calendar_range' label='Date' ariaLabel='Select date' />
             </Cell>
         </Row>
     </Grid>
@@ -43,7 +43,7 @@ export const Loading = () => (
                 <Paragraph margin='1rem 0 1rem 0'>Input search with loading prop</Paragraph>
             </Cell>
             <Cell xs={8} xsOffset={2}>
-                <Input type='text' placeholder='Search text' icon='search' isLoading />
+                <Input type='text' placeholder='Search text' icon='search' isLoading label='Search text' />
             </Cell>
         </Row>
     </Grid>
@@ -56,11 +56,11 @@ export const Disabled = () => (
                 <Paragraph margin='1rem 0 1rem 0'>Disabled input, both placeholders and values</Paragraph>
             </Cell>
             <Cell xs={12}>
-                <Input type='text' value='Value Text' disabled />
+                <Input type='text' value='Value Text' disabled label='Value' />
             </Cell>
 
             <Cell xs={12}>
-                <Input type='text' placeholder='Placeholder text' disabled />
+                <Input type='text' placeholder='Placeholder text' disabled label='Disabled input' />
             </Cell>
         </Row>
     </Grid>
