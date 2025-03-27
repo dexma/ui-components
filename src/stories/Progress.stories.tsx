4f50d80b99b134c7adcd3b8323ce1137716e742b
matias.pavan1 <matias.pavan@randstad.es>
2025-03-27 10:53:50 +0100

    feat(progress): in each example there is only a progress bar.

--- src/stories/Progress.stories.tsx
+++ src/stories/Progress.stories.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { Progress, Grid, Row, Cell, Paragraph, ProgressProps } from '@components';
 
 export default {
@@ -15,9 +14,6 @@ export const Basic = () => (
             </Cell>
             <Cell xs={12}>
                 <Progress percent={20} text='20%' color='gray' />
-                <Progress percent={40} text='40%' color='red' />
-                <Progress percent={60} text='60%' color='amber' />
-                <Progress percent={80} text='80%' color='green' />
             </Cell>
         </Row>
     </Grid>
@@ -30,10 +26,7 @@ export const ProgressTransparent = () => (
                 <Paragraph margin='1rem 0 1rem 0'>Display the current progress of an operation flow.</Paragraph>
             </Cell>
             <Cell xs={12}>
-                <Progress percent={20} text='20%' color='gray' isTransparent />
-                <Progress percent={40} text='40%' color='red' isTransparent />
-                <Progress percent={60} text='60%' color='amber' isTransparent />
-                <Progress percent={80} text='80%' color='green' isTransparent />
+                <Progress percent={20} text='20%' color='green' isTransparent />
             </Cell>
         </Row>
     </Grid>
@@ -52,8 +45,7 @@ export const Marks = () => (
                 <Paragraph margin='1rem 0 1rem 0'>Display the current progress of an operation flow.</Paragraph>
             </Cell>
             <Cell xs={12}>
-                <Progress percent={10} text='10%' color='red' marks={oneMark} />
-                <Progress percent={40} text='40%' color='green' marks={twoMark} />
+                <Progress percent={40} text='40%' color='blue' marks={twoMark} />
             </Cell>
         </Row>
     </Grid>
@@ -66,10 +58,7 @@ export const NoText = () => (
                 <Paragraph margin='1rem 0 1rem 0'>Display the current progress without text</Paragraph>
             </Cell>
             <Cell xs={12}>
-                <Progress percent={20} color='gray' />
-                <Progress percent={40} color='red' />
-                <Progress percent={60} color='amber' />
-                <Progress percent={80} color='green' />
+                <Progress percent={20} color='red' />
             </Cell>
         </Row>
     </Grid>
